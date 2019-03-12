4dda107228be6362b37641bf26ead70540a649a6
vagrant <tp.bowes@gmail.com>
2019-03-12 21:57:12 +0000

    implemented sample code to render new tweet

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,28 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 import Message from './Message.jsx';
 
-const data =
-    { currentUser: {
+// const data =
+//     { currentUser: {
+//       name: 'Bob'
+//     }, // optional. if currentUser is not defined, it means the user is Anonymous
+//     messages: [
+//     {
+//       id: 1,
+//       username: 'Bob',
+//       content: 'Has anyone seen my marbles?'
+//     },
+//     {
+//       id: 2,
+//       username: 'Anonymous',
+//       content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+//     }
+//   ]
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentUser: {
       name: 'Bob'
     }, // optional. if currentUser is not defined, it means the user is Anonymous
     messages: [
@@ -20,21 +40,29 @@ const data =
       content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
     }
   ]
-}
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { data };
+};
+  }
+  
+  componentDidMount() {
+    // console.log('componentDidMount <App />');
+    setTimeout(() => {
+      // console.log('Simulating incoming message');
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
   }
 
   render() {
     return (
       <div>
         <Navbar />
-        <MessageList messages={this.state.data.messages} />
+        <MessageList messages={this.state.messages} />
         <Message />
-        <ChatBar currentUser={this.state.data.currentUser} />
+        <ChatBar currentUser={this.state.currentUser} />
       </div>
     );
   }
