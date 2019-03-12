e113a5ac7908edd9d05fb5f6555b508e36a3f843
vagrant <tp.bowes@gmail.com>
2019-03-12 01:42:54 +0000

    split prewritten html/css and rendered base chattyApp

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,47 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Navbar />
+        <DisplayMessages />
+        <Chatbar />
+      </div>
     );
   }
 }
+
+class Navbar extends App {
+  render() {
+    return (
+      <nav className="navbar">
+         <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+    )
+  }}
+
+class DisplayMessages extends App {
+  render() {
+    return (
+      <main className="messages">
+        <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">I won't be impressed with technology until I can download food.</span>
+        </div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </main>
+    )
+  }}
+class Chatbar extends App {
+  render() {
+    return (
+      <footer className="chatbar">
+        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    )
+  }
+}
+ 
 export default App;
